a6a4d0084693ebba708bdc70c5e9f8b750386934
ktzazz <155502502+ktzazz@users.noreply.github.com>
2025-11-19 18:55:39 -0600

    script menu

--- script.js
+++ script.js
@@ -4,9 +4,36 @@ document.addEventListener("DOMContentLoaded", function () {
   const mobileClose = document.querySelector(".mobile__close");
   const mobileMenu = document.querySelector(".header__nav");
 
-  navBtn.addEventListener("click", function () {
+  function toggleMobileMenu() {
     mobileOpen.classList.toggle("inactive");
     mobileClose.classList.toggle("inactive");
     mobileMenu.classList.toggle("open");
-  });
+    document.body.classList.toggle("no-scroll");
+    logoColor();
+  }
+
+  function logoColor() {
+    const logo = document.querySelector(".bookmark");
+    const circle = document.querySelector(".circle");
+    const inner = document.querySelector(".innerCircle");
+
+    const rootStyles = getComputedStyle(document.documentElement);
+    const logoOg = rootStyles.getPropertyValue("--blue-950");
+    const circleOg = rootStyles.getPropertyValue("--blue");
+    const innerOg = rootStyles.getPropertyValue("--white");
+    const lightGray = rootStyles.getPropertyValue("--grey");
+    const bgMenu = rootStyles.getPropertyValue("--background-menu");
+
+    if (mobileMenu.classList.contains("open")) {
+      logo.style.fill = lightGray;
+      circle.style.fill = lightGray;
+      inner.style.fill = bgMenu;
+    } else {
+      logo.style.fill = logoOg;
+      circle.style.fill = circleOg;
+      inner.style.fill = innerOg;
+    }
+  }
+
+  navBtn.addEventListener("click", toggleMobileMenu);
 });
